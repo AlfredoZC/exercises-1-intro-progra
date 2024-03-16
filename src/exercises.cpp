1da5183dda09c90218a3b021d15116ae4a66fb5d
AlfredoZC <159178503+AlfredoZC@users.noreply.github.com>
2024-03-15 23:54:41 -0400

    .

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -153,6 +153,7 @@ void exercise_9(string s1, string s2, string s3, string s4, string s5) {
   char r_s5 = s5 [0];
 
   if (r_s1 == r_s5){
+
     cout<<r_s1<<r_s2<<r_s3<<r_s4<<r_s5<<endl;
     cout <<"Hemos encontrado algo!"<<endl;
   } else {
