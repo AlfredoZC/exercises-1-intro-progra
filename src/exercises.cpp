5c8ea072bbcecff2ab444e58f6fb5080165550d9
AlfredoZC <159178503+AlfredoZC@users.noreply.github.com>
2024-03-16 14:54:18 -0400

    Modifique el mensaje de salidad / exercise_15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -227,7 +227,7 @@ string exercise_14(int number_of_docs) {
 
 void exercise_15(int a, int b, int c) {
 
-  cout << "Los valores son:"<<"a = "<< a <<" b = "<<b<<" c = "<<c <<endl;
+  cout << "Los valores son:"<<"a = "<< a <<" b = "<<b<<" y "<<"c = "<<c <<endl;
     
   cout << "Permutamos: a => b, b => c, c => a"<<endl;
     
