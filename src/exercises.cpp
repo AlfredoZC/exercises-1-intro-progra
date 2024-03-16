8b5302241085f15365fba822b5c96c5389fa05da
AlfredoZC <159178503+AlfredoZC@users.noreply.github.com>
2024-03-16 17:27:28 -0400

    exercise_14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -228,8 +228,23 @@ string exercise_13(int age, int years_of_experience) {
 }
 
 string exercise_14(int number_of_docs) {
-  // TODO: YOUR CODE HERE
-  return "";
+
+string screen = to_string(number_of_docs);
+
+if (number_of_docs > 0){
+
+  if (number_of docs == 1){
+  return "Se encontro un documento";
+
+  } else if (number_of_docs > 1 ){
+    return screen + "documentos encontrados";
+  }
+
+} else {
+
+  return "No se encontraron documentos";
+}
+
 }
 
 void exercise_15(int a, int b, int c) {
